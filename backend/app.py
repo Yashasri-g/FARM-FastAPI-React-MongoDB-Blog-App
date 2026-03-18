8fcf310bb06dbb1030425512441c865d6a66489f
Yashasri-g <yashasrig34@gmail.com>
2026-03-18 12:30:59 +0530

    change in cors

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Allow CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_origins=["*"],  # React app URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
